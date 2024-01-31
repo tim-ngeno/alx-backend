31aefd618b9adaecb9c90840a8dff5c0d0a7a334
Tim Ngeno <timngeno424@gmail.com>
2024-01-31 10:51:39 +0300

    implement Least Frequently Used caching

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+""" LFU caching system """
+from collections import OrderedDict
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+    A caching system based on LEast Frequently Used algorithm
+    """
+
+    def __init__(self):
+        """
+        Initializes the LFUCache class, inherited from BaseCaching
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.frequency = {}
+
+    def put(self, key, item):
+        """
+        Assigns the value of item to cache_data based on the `key`
+        """
+        if key is None or item is None:
+            return
+
+        # Update frequency on put
+        self.frequency[key] = self.frequency.get(key, 0) + 1
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Find Least frequently used item(s)
+            min_frequency = min(self.frequency.values())
+            items_to_discard = [k for k, v in self.frequency.items() if
+                                v == min_frequency]
+
+            # If more than 1 item, use LRU
+            if len(items_to_discard) > 1:
+                lru_key = next(iter(self.cache_data))
+                items_to_discard = [lru_key]
+            for discard_key in items_to_discard:
+                del self.cache_data[discard_key]
+                del self.frequency[discard_key]
+                print('DISCARD: {}'.format(discard_key))
+
+        # Assign new item to cache
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns the value in cache_data linked to the key
+        """
+        if key is not None and key in self.cache_data:
+            # Updata frequency on access
+            self.frequency[key] += 1
+            return self.cache_data[key]
+        return None
