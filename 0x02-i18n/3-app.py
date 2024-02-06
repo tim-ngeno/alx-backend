c6747521da358dbd74e0058318d9fab10fa0beaf
Tim Ngeno <timngeno424@gmail.com>
2024-02-06 16:05:33 +0300

    import the underscore flash() '_' statement

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 """ Parameterize templates """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel()
