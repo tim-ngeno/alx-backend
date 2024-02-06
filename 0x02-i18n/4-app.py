00ceac042e5216ce01ad9bb5e697b3d91f5aa9db
Tim Ngeno <timngeno424@gmail.com>
2024-02-06 21:29:10 +0300

    get the locale from the args in request

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Parameterize templates and generate Translations """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A config class to house the accepted language codes for the app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Gets the language locale from request or uses the default behaviour
+    """
+    # Check if `locale` is present in request args
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+
+    # Resort to default if locale arg is not present
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Returns the index template
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
