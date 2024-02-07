4328e6c415e79b28e337c5a555c8c805e7f2118f
Tim Ngeno <timngeno424@gmail.com>
2024-02-07 13:02:05 +0300

    update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,8 +2,7 @@
 """ Basic Babel setup """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel
-from flask_babel import gettext as _
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel()
